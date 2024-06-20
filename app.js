89445c357d601dba064dc2334cb84ee234ccafb3
Bunma93 <bunma93@gmail.com>
2024-06-20 15:18:16 +0700

    Js-lab5

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // write code here
+let firstName = "ปฏิพงษศกร";
+let surName = "บุญมา";
+let age = 23;
+let address = "25/2 หมู่ 4 แขวงทวีวัฒนา เขตทวีวัฒนา กรุงเทพฯ 10170"
+console.log(`Full Name: ${firstName} ${surName}, Age: ${age}, Address: ${address}`)
